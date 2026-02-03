edeafd2fa170f5540f00297d01b308945bca555f
Ansuman <ansumanp@mindfiresolutions.com>
2026-02-03 21:48:25 +0530

    check husky commits

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,14 +1,8 @@
-import globals from 'globals';
-import tseslint from 'typescript-eslint';
-import { defineConfig } from 'eslint/config';
+import globals from "globals";
+import tseslint from "typescript-eslint";
+import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  {
-    ignores: ['./commitlint.config.cjs'],
-  },
-  {
-    files: ['**/*.{mjs,ts,mts,cts,jsx,tsx}'],
-    languageOptions: { globals: globals.browser },
-  },
+  { files: ["**/*.{js,mjs,cjs,ts,mts,cts}"], languageOptions: { globals: globals.browser } },
   tseslint.configs.recommended,
 ]);
