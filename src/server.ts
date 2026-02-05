7ba9e6c083b7b97a492ecb9d84bdad0ee1924040
Ansuman <ansumanp@mindfiresolutions.com>
2026-02-05 17:40:44 +0530

    feat: configured NGINX with docker containers

--- src/server.ts
+++ src/server.ts
@@ -17,6 +17,6 @@ app.use('/api', blogRoutes);
 app.use('/users', adminRoutes);
 app.use('/users/:userId/roles', roleRoutes);
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`server is running on port ${PORT}`);
 });
